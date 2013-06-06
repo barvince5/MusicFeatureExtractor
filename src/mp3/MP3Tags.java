35fc87c3456c626b82d5e75f58d2c415e04778a7
AntonioCollarino <anto.collarino@gmail.com>
2013-06-07 00:48:59 +0200

    fixed xsd, and added some methods inside MP3Tags class.

--- src/mp3/MP3Tags.java
+++ src/mp3/MP3Tags.java
@@ -1,6 +1,10 @@
 package mp3;
 
 import java.io.File;
+import java.math.BigInteger;
+
+import songArtifacts.ObjectFactory;
+import songArtifacts.SongMetadataType;
 
 import customException.MP3Exception;
 
@@ -13,6 +17,7 @@ public final class MP3Tags {
 	
 	private static Tag tag= null;
 	private static AudioFile audioFile= null;
+	private static SongMetadataType smt;
 	
 	private final static Tag getTags(File song) 
 			throws MP3Exception {
@@ -21,7 +26,7 @@ public final class MP3Tags {
 			throw new MP3Exception("The song is null");
 		
 		try {
-			audioFile = AudioFileIO.read(song);
+			MP3Tags.audioFile = AudioFileIO.read(song);
 			MP3Tags.tag= audioFile.getTag();
 			
 		} catch (CannotReadException e) {
@@ -30,12 +35,57 @@ public final class MP3Tags {
 			throw new MP3Exception("Exception "+e.getMessage(), e);
 		}
 		
-		return MP3Tags.tag;
+		return MP3Tags.tag;		
+	}
+	
+	private final static void setAudioFile() 
+			throws MP3Exception {
 		
+		try {
+			MP3Tags.smt.setName(MP3Tags.audioFile.getName()); //file name.
+			MP3Tags.smt.setBitrate(BigInteger.valueOf(MP3Tags.audioFile.getBitrate())); //KB/s
+			MP3Tags.smt.setChannelsNum(BigInteger.valueOf(MP3Tags.audioFile.getChannelNumber()));
+			MP3Tags.smt.setEncoding(MP3Tags.audioFile.getEncodingType());
+			MP3Tags.smt.setFrequency(BigInteger.valueOf(MP3Tags.audioFile.getSamplingRate())); //freq. in hz
+			MP3Tags.smt.setLength(BigInteger.valueOf(MP3Tags.audioFile.getLength())); // seconds
+		} catch (Exception e) {
+			throw new MP3Exception("Exception during setAudioFile");
+		}
+	}
+	
+	private final static void setMp3Tag() 
+			throws MP3Exception {
 		
-//		System.err.println("Ar "+audioFile.getTag().getFirstArtist());
-//		System.err.println("til "+audioFile.getTag().getFirstTitle());
-//		System.err.println("al "+audioFile.getTag().getFirstAlbum());
+		try {
+			String songTitle= MP3Tags.audioFile.getTag().getFirstTitle();
+			String albumName= MP3Tags.audioFile.getTag().getFirstAlbum();
+			String artistName= MP3Tags.audioFile.getTag().getFirstArtist();
+			MP3Tags.smt.getArtist().setName(artistName);
+			
+			//TODO
+			//take xml from musicbrainz
+			
+		} catch (Exception e) {
+			throw new MP3Exception("Exception during setMp3Tag");
+		}
+	}
+	
+	public final static boolean storeMP3Tags(File song, ObjectFactory obf) 
+			throws MP3Exception {
 		
+		if(song == null || obf == null)
+			throw new MP3Exception("The song and/or objectfactory is null");
+
+		try {
+			MP3Tags.smt= obf.createSongMetadataType();
+			MP3Tags.smt.setArtist(obf.createArtistType());
+			MP3Tags.getTags(song);
+			MP3Tags.setAudioFile();
+			MP3Tags.setMp3Tag();
+		} catch (Exception e) {
+			return false;
+		}
+		
+		return true;
 	}
 }
