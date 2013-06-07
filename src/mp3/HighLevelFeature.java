645a9e795c6bc34a79c9e8a6c8ef2edca006bbfd
AntonioCollarino <anto.collarino@gmail.com>
2013-06-07 20:19:44 +0200

    new schemas and artifacts.

---
+++ src/mp3/HighLevelFeature.java
@@ -0,0 +1,63 @@
+package mp3;
+
+import songArtifacts.highLevel.ArtistType;
+import songArtifacts.highLevel.ObjectFactory;
+import songArtifacts.highLevel.SongMetadataType;
+
+import java.io.File;
+import java.math.BigInteger;
+
+
+import customException.MP3Exception;
+import entagged.audioformats.AudioFile;
+
+public final class HighLevelFeature extends MP3Info {
+
+	private SongMetadataType smt= null;
+	private ObjectFactory obf;
+	
+	public HighLevelFeature(File song) 
+			throws MP3Exception {
+		
+		super(song);
+		this.obf= new ObjectFactory();
+		this.smt= this.obf.createSongMetadataType();
+	}
+
+	public final void start() 
+			throws MP3Exception {
+		
+		this.setAudioFile();
+	}
+	
+	public final void stop() {
+		
+	}
+	
+	private final void setAudioFile() 
+			throws MP3Exception {
+		
+		try { 
+			
+			AudioFile audioFile= super.getAudioFile();
+			ArtistType artist= this.obf.createArtistType();
+			artist.setName(audioFile.getName());
+			
+			//file name can be different of the title. 
+			this.smt.setArtist(artist);
+			//KB/s
+			this.smt.setBitrate(BigInteger.valueOf(audioFile.getBitrate()));
+			//1 mono, 2 stereo
+			this.smt.setChannelsNum(BigInteger.valueOf(audioFile.getChannelNumber()));
+			this.smt.setEncoding(audioFile.getEncodingType());
+			//freq. in hz
+			this.smt.setFrequency(BigInteger.valueOf(audioFile.getSamplingRate()));
+			// seconds
+			this.smt.setLength(BigInteger.valueOf(audioFile.getLength())); 
+			
+		} catch (Exception e) {
+			throw new MP3Exception("Exception during setAudioFile");
+		}
+	}
+
+}
---
+++ src/mp3/MP3Info.java
@@ -0,0 +1,69 @@
+package mp3;
+
+import java.io.File;
+
+import customException.MP3Exception;
+import customException.UnsupportedAudioFileException;
+import entagged.audioformats.AudioFile;
+import entagged.audioformats.AudioFileIO;
+import entagged.audioformats.Tag;
+import entagged.audioformats.exceptions.CannotReadException;
+
+public class MP3Info {
+	
+	private Tag tag= null;
+	private AudioFile audioFile= null;
+	private String songTitle= null;
+	private String albumName= null;
+	private String artistName= null;
+	
+	public MP3Info(File song) 
+			throws MP3Exception {
+		
+		if(song == null)
+			throw new MP3Exception("The song is null");
+		
+		try {
+			this.getMP3Tag(song);
+		} catch (UnsupportedAudioFileException e) {
+			throw new MP3Exception("UnsupportedAudioFileException "+e.getMessage(), e);
+		}
+	}
+	
+	private final void getMP3Tag(File song) 
+			throws MP3Exception, UnsupportedAudioFileException {
+		
+		try {
+			this.audioFile = AudioFileIO.read(song);
+			this.tag= audioFile.getTag();
+			// all of these values are just stored in the attributes of this class.
+			this.songTitle= this.tag.getFirstTitle();
+			this.albumName= this.tag.getFirstAlbum();
+			this.artistName= this.tag.getFirstArtist();
+		} catch (CannotReadException e) {
+			throw new UnsupportedAudioFileException("CannotReadException "+e.getMessage(), e);
+		} catch (Exception e) {
+			throw new MP3Exception("Exception during setMp3Tag");
+		}
+	}
+	
+	public final String getTitle() {
+		return this.songTitle;
+	}
+	
+	public final String getAlbum() {
+		return this.albumName;
+	}
+	
+	public final String getArtist() {
+		return this.artistName;
+	}
+
+	public final Tag getTags() {
+		return this.tag;
+	}
+	
+	public final AudioFile getAudioFile() {
+		return this.audioFile;
+	}
+}
