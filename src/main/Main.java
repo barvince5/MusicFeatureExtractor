bf7c7192a27f120c1ecd300b4382afc320b63123
AntonioCollarino <anto.collarino@gmail.com>
2013-06-09 11:07:53 +0200

    fixed bug in artist. Added custom event handler for marshaller

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,10 @@
 package main;
 
+import java.io.File;
+import java.io.FilenameFilter;
+
+import mp3.ArtistFeature;
+
 import customException.MP3Exception;
 
 
@@ -8,6 +13,19 @@ public class Main {
 
 	public static void main(String[] args) throws MP3Exception {
 		
+		File dir= new File("/home/sniper/Desktop/music/");
+		File[] foundFiles = dir.listFiles(new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String name) {
+				return name.endsWith(".mp3");
+			}
+		});
+		
+		for(File f : foundFiles) {
+			ArtistFeature ar= new ArtistFeature(f);
+			ar.start();
+			System.out.println(f.getName());
+		}
 	}
 
 }
