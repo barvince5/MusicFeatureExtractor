556205b6bb0ddba3e6f212ad70a7423b75af26cb
AntonioCollarino <anto.collarino@gmail.com>
2013-06-14 19:21:31 +0200

    modified classpath

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,28 +1,40 @@
 package main;
 
 
+import java.util.HashMap;
+import java.util.Iterator;
+
 import customException.MasterException;
+import feature.lowLevel.DataModel;
 
 public class Main {
 
 
 	public static void main(String[] args) {
 		
-		try {
-			
-			System.err.println("ARTISTS PHASE: Please wait...");
-			MasterMetadata.artistMetadata("/home/sniper/Desktop/music/");
+//		try {
+//			
+//			System.err.println("ARTISTS PHASE: Please wait...");
+//			MasterMetadata.artistMetadata("/home/sniper/Desktop/music/");
+//		
+//			System.err.println("ALBUMS PHASE: Please wait...");
+//			MasterMetadata.albumMetadata("/home/sniper/Desktop/music/");
+//			
+//			System.err.println("SONGS PHASE: Please wait...");
+//			MasterMetadata.songMetadata("/home/sniper/Desktop/music/");
+//			
+//		} catch (MasterException e) {
+//			System.err.println(e.getMessage());
+//			MasterMetadata.shutDownMFE();
+//			System.err.println("MFE is shutting down");
+//		}
 		
-			System.err.println("ALBUMS PHASE: Please wait...");
-			MasterMetadata.albumMetadata("/home/sniper/Desktop/music/");
-			
-			System.err.println("SONGS PHASE: Please wait...");
-			MasterMetadata.songMetadata("/home/sniper/Desktop/music/");
-			
-		} catch (MasterException e) {
+		try {
+			DataModel dm= new DataModel();
+			dm.setFile("/home/sniper/Desktop/music/aaa.mp3");
+			dm.extract();
+		} catch(Exception e) {
 			System.err.println(e.getMessage());
-			MasterMetadata.shutDownMFE();
-			System.err.println("MFE is shutting down");
 		}
 	}
 }
